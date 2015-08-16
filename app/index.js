43244e0585b4de14514b884dd2c84d4f01f89096
Kurt Bartholomew <siever246@gmail.com>
2015-08-16 10:33:04 -0700

    (feat) Added proper building of structure

--- app/index.js
+++ app/index.js
@@ -15,7 +15,7 @@ module.exports = generators.Base.extend({
     },
     git: function(){
       this.fs.copy(
-        this.templatePath('gitignore'),
+        this.templatePath('_.gitignore'),
         this.destinationPath('.gitignore'));
     },
     packageJSON: function(){
@@ -24,16 +24,89 @@ module.exports = generators.Base.extend({
         this.destinationPath('package.json'));
     },
     bower: function(){
+      var bowerJson = {
+        name: "newapp",
+        private: true,
+        dependencies: {}
+      };
+
+      bowerJson.dependencies['normalize'] = "~1.0.4";
+
+      this.fs.writeJSON('bower.json', bowerJson);
       this.fs.copy(
         this.templatePath('_.bowerrc'),
         this.destinationPath('.bowerrc'));
     },
-    server: function(){
-      // figure this out
-    },
     client: function(){
-      // figure this out
+      this.fs.copy(
+        this.templatePath('client/src/app/app.js'),
+        this.destinationPath('client/src/app/app.js'));
+      this.fs.copy(
+        this.templatePath('client/src/favicon.ico'),
+        this.destinationPath('client/src/favicon.ico'));
+      this.fs.copy(
+        this.templatePath('client/src/robots.txt'),
+        this.destinationPath('client/src/robots.txt'));
+      this.fs.copy(
+        this.templatePath('client/src/index.html'),
+        this.destinationPath('client/src/index.html'));
+
+      mkdirp('client/src/assets/css');
+      mkdirp('client/src/assets/fonts');
+      mkdirp('client/src/assets/images');
+      mkdirp('client/src/assets/js');
+      mkdirp('client/src/assets/lib');
+    },
+    e2e: function(){
+      this.fs.copy(
+        this.templatePath('e2e/main.spec.js'),
+        this.destinationPath('e2e/main.spec.js'));
+    },
+    server: function(){
+      this.fs.copy(
+        this.templatePath('server/api/sample/sampleController.js'),
+        this.destinationPath('server/api/sample/sampleController.js'));
+      this.fs.copy(
+        this.templatePath('server/api/sample/sampleRoutes.js'),
+        this.destinationPath('server/api/sample/sampleRoutes.js'));
+      this.fs.copy(
+        this.templatePath('server/bin/www/index.js'),
+        this.destinationPath('server/bin/www/index.js'));
+      this.fs.copy(
+        this.templatePath('server/config/environment/index.js'),
+        this.destinationPath('server/config/environment/index.js'));
+      this.fs.copy(
+        this.templatePath('server/config/db.js'),
+        this.destinationPath('server/config/db.js'));
+      this.fs.copy(
+        this.templatePath('server/config/middleware.js'),
+        this.destinationPath('server/config/middleware.js'));
+      this.fs.copy(
+        this.templatePath('server/models/sampleModel.js'),
+        this.destinationPath('server/models/sampleModel.js'));
+      this.fs.copy(
+        this.templatePath('server/test/sample.spec.js'),
+        this.destinationPath('server/test/sample.spec.js'));
+      this.fs.copy(
+        this.templatePath('server/server.js'),
+        this.destinationPath('server/server.js'));
+      this.fs.copy(
+        this.templatePath('server/.jshintrc'),
+        this.destinationPath('server/.jshintrc'));
+
+      mkdirp('server/views');
     }
+  },
+  end: function(){
+    var bowerJson = this.fs.readJSON(this.destinationPath('bower.json'));
+
+    wiredep({
+      bowerJson: bowerJson,
+      directory: 'client/src/assets/lib',
+      exclude: ['bootstrap-sass', 'bootstrap.js'],
+      ignorePath: /^(\.\.\/)*\.\./,
+      src: 'client/src/index.html'
+    });
   }
 });
 
